5ff975e3c54e910a809908fc04cadb524938f848
pawel <pawel@DESKTOP-QCHVK5T>
2017-01-23 15:46:57 +0100

    Commit

---
+++ eProto/eProto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eProto.Startup))]
+namespace eProto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
