10e6bac31712006376f8b7b57d43e6d9aca364fc
pawel <pawel@DESKTOP-QCHVK5T>
2017-01-25 21:05:17 +0100

    Departments

--- eProto/eProto/DAL/SchoolContext.cs
+++ eProto/eProto/DAL/SchoolContext.cs
@@ -22,6 +22,7 @@ namespace eProto.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
